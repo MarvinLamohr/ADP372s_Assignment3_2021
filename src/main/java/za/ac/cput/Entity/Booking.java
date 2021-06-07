a23b8a028623bf595b9ee3d6564517f12848284d
Wajedien Samuels <67780251+Ianlowkey@users.noreply.github.com>
2021-06-07 23:15:20 +0200

    Booking entity

---
+++ src/main/java/za/ac/cput/Entity/Booking.java
@@ -0,0 +1,91 @@
+package za.ac.cput.Entity;
+
+/*  Booking.java
+ *  Entity for the booking
+ *  Author: Ian Louw (216250773)
+ *  Date: 7 June 2021
+ */
+
+public class Booking {
+    private String name, address, database, date, status;
+    private Double price;
+
+    private Booking(Builder builder){
+        this.name = builder.name;
+        this.address = builder.address;
+        this.database = builder.database;
+        this.date = builder.date;
+        this.price = builder.price;
+        this.status = builder.status;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", database='" + database + '\'' +
+                ", date='" + date + '\'' +
+                ", status='" + status + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public static class Builder{
+
+        private String name, address, database, date, status;
+        private Double price;
+
+        public Builder setName(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+
+            return this;
+        }
+
+        public Builder setDatabase(String database) {
+            this.database = database;
+
+            return this;
+        }
+
+        public Builder setDate(String date) {
+            this.date = date;
+
+            return this;
+        }
+
+        public Builder setStatus(String status) {
+            this.status = status;
+
+            return this;
+        }
+
+        public Builder setPrice(Double price) {
+            this.price = price;
+
+            return this;
+        }
+
+        public Booking build () {
+
+            return new Booking(this);
+        }
+
+        public Builder copy(Booking booking) {
+            this.name = booking.name;
+            this.address = booking.address;
+            this.database = booking.database;
+            this.date = booking.date;
+            this.price = booking.price;
+            this.status = booking.status;
+
+            return this;
+        }
+    }
+}
