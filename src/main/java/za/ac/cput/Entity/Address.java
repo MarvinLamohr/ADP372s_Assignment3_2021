e8fa9f6611afb2bfe1d3f734dc72d0f5a6326282
Mawande05 <Mawande>
2021-06-10 13:35:22 +0200

    First commit, entity and factory.

---
+++ src/main/java/za/ac/cput/Entity/Address.java
@@ -0,0 +1,51 @@
+/* Address.java
+    Entity for the Address
+    Author: Mawande Langa (219074054)
+    Date: 11/06/2021
+
+ */
+package za.ac.cput.Entity;
+
+public class Address {
+
+    private  String streetAddress,postalAddress;
+
+
+    private Address(Builder builder) {
+        this.streetAddress = builder.streetAddress;
+        this.postalAddress = builder.postalAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "streetAddress='" + streetAddress + '\'' +
+                ", postalAddress='" + postalAddress + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+
+        private  String streetAddress,postalAddress;
+
+        public Builder setStreetAddress(String streetAddress) {
+            this.streetAddress = streetAddress;
+            return this;
+        }
+
+        public Builder setPostalAddress(String postalAddress) {
+            this.postalAddress = postalAddress;
+            return this;
+        }
+
+        public Address build(){
+            return new Address(this);
+        }
+
+        public Builder copy(Address address){
+            this.streetAddress = address.streetAddress;
+            this.postalAddress = address.postalAddress;
+            return this;
+        }
+    }
+}
---
+++ src/main/java/za/ac/cput/Entity/Venue.java
@@ -0,0 +1,65 @@
+/* Address.java
+    Entity for the Address
+    Author: Mawande Langa (219074054)
+    Date: 11/06/2021
+
+ */
+
+package za.ac.cput.Entity;
+
+public class Venue {
+    private String venueName,venueLocation;
+    private int venueCapacity;
+
+    private Venue(Builder builder) {
+        this.venueName = builder.venueName;
+        this.venueCapacity = builder.venueCapacity;
+        this.venueLocation = builder.venueLocation;
+    }
+
+    @Override
+    public String toString() {
+        return "Venue{" +
+                "venueName='" + venueName + '\'' +
+                ", venueCapacity='" + venueCapacity + '\'' +
+                ", venueLocation='" + venueLocation + '\'' +
+                '}';
+    }
+
+
+    public static class Builder{
+        private String venueName,venueLocation;
+        private int venueCapacity;
+
+
+        public Builder setVenueName(String venueName) {
+            this.venueName = venueName;
+            return this;
+        }
+
+        public Builder setVenueCapacity(int venueCapacity) {
+            this.venueCapacity = venueCapacity;
+            return this;
+        }
+
+        public Builder setVenueLocation(String venueLocation) {
+            this.venueLocation = venueLocation;
+            return this;
+        }
+
+
+
+        public Venue build(){
+            return new Venue(this);
+        }
+
+        public Builder copy(Venue venue){
+
+            this.venueName = venue.venueName;
+            this.venueCapacity = venue.venueCapacity;
+            this.venueLocation = venue.venueLocation;
+
+            return this;
+        }
+    }
+}
