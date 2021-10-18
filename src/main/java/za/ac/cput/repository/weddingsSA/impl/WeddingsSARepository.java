d1f610433e94513961552d2e93b5be91872f8f0a
lefukumeke123 <kumekel3310@gmail.com>
2021-10-18 19:44:23 +0200

    Repository update

--- src/main/java/za/ac/cput/repository/weddingsSA/impl/WeddingsSARepository.java
+++ src/main/java/za/ac/cput/repository/weddingsSA/impl/WeddingsSARepository.java
@@ -2,8 +2,11 @@ package za.ac.cput.repository.weddingsSA.impl;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import za.ac.cput.entity.WeddingsSA;
 
+
+@Repository
 public interface WeddingsSARepository   extends JpaRepository<WeddingsSA, String> {
 
 
